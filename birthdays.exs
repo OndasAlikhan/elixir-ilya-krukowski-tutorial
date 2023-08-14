d91d5a086a216ac35c68124e796a717cd6f91f23
Alikhan Ondas <alihan.ondas@gmail.com>
2023-08-14 23:14:34 +0600

    add long running process in birthdays.ex

--- birthdays.exs
+++ birthdays.exs
@@ -1,14 +1,58 @@
 defmodule Birthdays.Server do
   def run do
-    spawn fn -> loop() end
+    Kernel.spawn(fn -> loop(%{}) end) |> Process.register(:birthdays)
   end
 
-  defp loop do
-    receive do
-
+  defp loop(current_state) do
+    new_state = receive do
+      message -> process_message(current_state, message)
     end
 
-    loop()
+    new_state |> loop()
   end
 
+  defp process_message(current_state, { :add, name, birthday }) do
+    current_state |> Map.put(name, birthday)
+  end
+  defp process_message(current_state, { :all, caller }) do
+    caller |> send({ :response, current_state })
+    current_state
+  end
+  defp process_message(current_state, { :get, caller, name }) do
+    caller |> send({ :response, current_state[name] })
+    current_state
+  end
+  defp process_message(current_state, { :remove, name }) do
+    current_state |> Map.delete(name)
+  end
 end
+
+defmodule Birthdays.Client do
+  @birthdays_pid :birthdays
+
+  def all do
+    do_send({ :all, self() })
+    get_response()
+  end
+  def get(name) do
+    do_send({ :get, self(), name })
+    get_response()
+  end
+  def add(name, birthday), do: do_send({ :add, name, birthday })
+  def remove(name), do: do_send({ :remove, name })
+
+
+  def do_send(msg), do: send(@birthdays_pid, msg)
+
+  defp get_response() do
+    receive do
+      {:response, data} -> data
+      _ -> :error
+      after 5000 -> IO.puts("Timeout!")
+    end
+  end
+end
+
+Birthdays.Server.run
+
+# alias Birthdays.Client
